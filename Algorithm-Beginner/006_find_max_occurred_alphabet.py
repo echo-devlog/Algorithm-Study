ce38e96d1eb280902284815c5fd3c1c4a9195d8f
HAKYUNG SONG <echo.devlog@gmail.com>
2025-06-22 23:58:37 +0900

    docs: 빈도수가 많은 알파벳을 찾는 두 가지 방법 중에 어느 함수가 더 효율적일지 고민

--- Algorithm-Beginner/006_find_max_occurred_alphabet.py
+++ Algorithm-Beginner/006_find_max_occurred_alphabet.py
@@ -2,22 +2,23 @@ input = "hello my name is sparta"
 
 
 def find_max_occurred_alphabet(string):
-    alphabet_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    alphabet_occurrence_array = [0] * 26
+
+    for char in string:
+        if not char.isalpha():
+            continue
+        arr_index = ord(char) - ord("a")
+        alphabet_occurrence_array[arr_index] += 1
 
     max_occurrence = 0
-    max_alphabet = alphabet_array[0]
+    max_alphabet_index = 0
+    for index in range(len(alphabet_occurrence_array)):
+        alphabet_occurrence = alphabet_occurrence_array[index]
 
-    for alphabet in alphabet_array:
-        occurrence = 0
-        for char in string:
-            if char == alphabet:
-                occurrence += 1
-
-        if occurrence > max_occurrence:
-            max_occurrence = occurrence
-            max_alphabet = alphabet
-
-    return max_alphabet
+        if alphabet_occurrence > max_occurrence:
+            max_alphabet_index = index
+            max_occurrence = alphabet_occurrence
+    return chr(max_alphabet_index + ord("a"))
 
 
 result = find_max_occurred_alphabet(input)
