c87c75f93f0b26eb58d3157c6ffc7ecff3babaf1
HAKYUNG SONG <echo.devlog@gmail.com>
2025-06-27 00:03:35 +0900

    fix: 더하기 혹은 곱하기 최대값 찾기

--- Algorithm-Beginner/008_find_max_plus_or_multiply.py
+++ Algorithm-Beginner/008_find_max_plus_or_multiply.py
@@ -1,7 +1,6 @@
 # input = [0, 3, 5, 6, 1, 2, 4]
 input = [3, 5, 6, 1, 2, 4]
 
-
 def find_max_plus_or_multiply(array):
     multiply_sum = 0
     for number in array:
@@ -9,7 +8,7 @@ def find_max_plus_or_multiply(array):
             multiply_sum += number
         else:
             multiply_sum *= number
-        return multiply_sum
+    return multiply_sum
 
 
 result = find_max_plus_or_multiply(input)
